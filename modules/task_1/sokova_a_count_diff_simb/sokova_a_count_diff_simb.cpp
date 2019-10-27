1cf65483a48f25e174644ac1457fd2aba75399e1
wappah <56585074+wappah@users.noreply.github.com>
2019-10-27 23:17:12 +0300

    Update sokova_a_count_diff_simb.cpp

--- modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.cpp
+++ modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.cpp
@@ -1,3 +1,4 @@
+// Copyright 2019 Sokova Anastasia
 #include "../../../modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.h";
 #include <cstring>
 #include <random>
@@ -14,14 +15,13 @@ int GetCountDiffSimb(char* Str1, char* Str2) {
   int Surplus = 0;
 
   if (strlen(Str1) <= strlen(Str2)) {
-	Step = strlen(Str1) / ProcSize;
-	Remainder = strlen(Str1) % ProcSize;
-	Surplus = strlen(Str2) - strlen(Str1);
-  }
-  else {
-	Step = strlen(Str2) / ProcSize;
-	Remainder = strlen(Str2) % ProcSize;
-	Surplus = strlen(Str1) - strlen(Str2);
+    Step = strlen(Str1) / ProcSize;
+    Remainder = strlen(Str1) % ProcSize;
+    Surplus = strlen(Str2) - strlen(Str1);
+  } else {
+    Step = strlen(Str2) / ProcSize;
+    Remainder = strlen(Str2) % ProcSize;
+    Surplus = strlen(Str1) - strlen(Str2);
   }
 
   int TempResult = 0;
@@ -39,25 +39,24 @@ int GetCountDiffSimb(char* Str1, char* Str2) {
   }
 
   if (ProcRank == 0) {
-	delete[] TempStr1;
-	delete[] TempStr2;
-	TempStr1 = new char[Step + Remainder + 1];
-	TempStr2 = new char[Step + Remainder + 1];
-	for (int i = 0; i < Step + Remainder; i++) {
-	  TempStr1[i] = Str1[i];
-	  TempStr2[i] = Str2[i];
-	}
-	TempStr1[Step + Remainder] = 0;
-	TempStr2[Step + Remainder] = 0;
-  }
-  else {
-	MPI_Status status;
-	if (Step > 0) {
-	  MPI_Recv(&TempStr1[0], Step, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-	  MPI_Recv(&TempStr2[0], Step, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
-	  TempStr1[Step] = 0;
-	  TempStr2[Step] = 0;
-	}
+    delete[] TempStr1;
+    delete[] TempStr2;
+    TempStr1 = new char[Step + Remainder + 1];
+    TempStr2 = new char[Step + Remainder + 1];
+    for (int i = 0; i < Step + Remainder; i++) {
+      TempStr1[i] = Str1[i];
+      TempStr2[i] = Str2[i];
+    }
+    TempStr1[Step + Remainder] = 0;
+    TempStr2[Step + Remainder] = 0;
+  } else {
+    MPI_Status status;
+    if (Step > 0) {
+      MPI_Recv(&TempStr1[0], Step, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&TempStr2[0], Step, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
+      TempStr1[Step] = 0;
+      TempStr2[Step] = 0;
+    }
   }
 
   TempResult = Count(TempStr1, TempStr2);
@@ -74,8 +73,8 @@ int GetCountDiffSimb(char* Str1, char* Str2) {
 int Count(char* Str1, char* Str2) {
   int Result = 0;
   for (size_t i = 0; i < strlen(Str1); i++) {
-	if (Str1[i] != Str2[i])
-	Result++;
+    if (Str1[i] != Str2[i])
+    Result++;
   }
   return Result;
 }
@@ -84,8 +83,8 @@ int GetRandomStr(char* Str, int StrSize) {
   const char Arr[] = "abcdefghijklmnopqrstuvwxyz0123456789";
 
   if (StrSize <= 0)
-	throw "Error. Invalid size.";
+    throw "Error. Invalid size.";
   for (int i = 0; i < StrSize; i++)
-	Str[i] = Arr[rand() % (sizeof(Arr) - 1)];
+    Str[i] = Arr[rand() % (sizeof(Arr) - 1)];
   Str[StrSize] = 0;
 }
