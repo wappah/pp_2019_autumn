481a66361d81c283e322b76cd4f4d93220b51889
wappah <56585074+wappah@users.noreply.github.com>
2019-10-27 23:23:31 +0300

    Update sokova_a_count_diff_simb.h

--- modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.h
+++ modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.h
@@ -1,9 +1,10 @@
-#ifndef MODULES_TASK_1_SOKOVA_A_COUNT_DIFF_SIMB_H_
-#define MODULES_TASK_1_SOKOVA_A_COUNT_DIFF_SIMB_H_
+// Copyright 2019 Sokova Anastasia
+#ifndef MODULES_TASK_1_SOKOVA_A_COUNT_DIFF_SIMB_SOKOVA_A_COUNT_DIFF_SIMB_H_
+#define MODULES_TASK_1_SOKOVA_A_COUNT_DIFF_SIMB_SOKOVA_A_COUNT_DIFF_SIMB_H_
 #include <mpi.h>
 #include <stdio.h>
 
 int GetCountDiffSimb(char* Str1, char* Str2);
 int Count(char* Str1, char* Str2);
 int GetRandomStr(char* Str, int Size);
-#endif  // MODULES_TASK_1_SOKOVA_A_COUNT_DIFF_SIMB_H_
+#endif  // MODULES_TASK_1_SOKOVA_A_COUNT_DIFF_SIMB_SOKOVA_A_COUNT_DIFF_SIMB_H_
