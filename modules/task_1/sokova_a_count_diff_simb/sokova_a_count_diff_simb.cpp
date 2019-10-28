671780dcfa2aa0d3b8cb5377e10eacc09dcdf0e2
wappah <56585074+wappah@users.noreply.github.com>
2019-10-28 11:06:14 +0300

    Update sokova_a_count_diff_simb.cpp

--- modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.cpp
+++ modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.cpp
@@ -1,9 +1,9 @@
 // Copyright 2019 Sokova Anastasia
-#include "../../../modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.h";
 #include <cstring>
 #include <random>
 #include <stdio.h>
 #include <iostream>
+#include "../../../modules/task_1/sokova_a_count_diff_simb/sokova_a_count_diff_simb.h";
 
 int GetCountDiffSimb(char* Str1, char* Str2) {
   int ProcSize, ProcRank;
